d31f4e0256b7c1aa7ba795cadb3f61d9d6815418
yigituncayy <yigituncayy@gmail.com>
2023-05-07 15:32:37 +0300

    deneme

--- script.js
+++ script.js
@@ -9,35 +9,4 @@ menuToggle.addEventListener('click', () => {
 // Contact form validation
 const form = document.querySelector('form');
 const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const messageInput = document.querySelector('#message');
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  let errorMessage = '';
-
-  if (nameInput.value === '') {
-    errorMessage += 'Name is required.\n';
-  }
-
-  if (emailInput.value === '') {
-    errorMessage += 'Email is required.\n';
-  } else if (!isValidEmail(emailInput.value)) {
-    errorMessage += 'Please enter a valid email address.\n';
-  }
-
-  if (messageInput.value === '') {
-    errorMessage += 'Message is required.\n';
-  }
-
-  if (errorMessage !== '') {
-    alert(errorMessage);
-  } else {
-    form.submit();
-  }
-});
-
-function isValidEmail(email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
+const emailInput = document.querySelector
